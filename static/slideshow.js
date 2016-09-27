5ff73ad00db2816bfb4763c0742c8ecd7c9692c9
Aanand Prasad <aanand.prasad@gmail.com>
2016-09-27 10:04:29 +0100

    Slideshow

---
+++ static/slideshow.js
@@ -0,0 +1,53 @@
+var numSlides = 1000;
+var slideIndex = randomInteger(1, numSlides);
+
+var maxScrolls = 3;
+var scrollIndex = 0;
+
+$(function() {
+  advance();
+  setInterval(advance, 10*1000);
+});
+
+function advance() {
+  var slideshow = $('#slideshow');
+  var body = slideshow[0].contentWindow.document.body;
+
+  if (scrollIndex < maxScrolls && body.clientHeight*2 < body.scrollHeight) {
+    scrollIndex += 1;
+    var targetHeight = (body.scrollHeight - body.clientHeight) * scrollIndex/maxScrolls;
+    $(body).animate({scrollTop: targetHeight}, '500');
+    return;
+  }
+
+  scrollIndex = 0;
+  nextSlide();
+}
+
+function nextSlide() {
+  var pageNumber = zeroPad(String(slideIndex), 5);
+  var path = '../assembled/' + sha1(pageNumber) + '.html';
+  $('#slideshow').attr('src', path);
+
+  slideIndex += 1;
+  if (slideIndex > numSlides) {
+    slideIndex = 1;
+  }
+}
+
+function sha1(string) {
+  var sha = new jsSHA("SHA-1", "BYTES");
+  sha.update(string);
+  return sha.getHash("HEX");
+}
+
+function zeroPad(string, length) {
+  while (string.length < length) {
+    string = "0" + string;
+  }
+  return string;
+}
+
+function randomInteger(min, max) {
+  return Math.floor(Math.random() * (max-min)) + min;
+}
